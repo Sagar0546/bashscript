b7f045325a10be7d0b1ab5c7b45827cbf978c6e4
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-25 19:29:45 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,11 @@
-sudo yum install python3-pip
-git clone https://github.com/k-vamc/fish.git
-cd fish
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
+sudo yum install -y git docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/k-vamc/wordpress
+cd wordpress
+docker-compose up -d
+
+
