eabdb76636e59da56f814257f36133dcd3131c86
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-25 19:52:17 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,5 +6,5 @@ sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/k-vamc/wordpress.git
-cd wordpress
+cd /home/ec2-user/workspace/slave1/bashscript/wordpress
 docker-compose up -d
