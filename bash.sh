edab1a8648c0902e39fa57507f400d273e1bf6bd
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-25 22:30:30 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,6 +4,7 @@ sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
+sudo usermod -aG docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/k-vamc/wordpress1.git
